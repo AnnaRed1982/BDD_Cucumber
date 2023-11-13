7233b37ddfaa5704aa0efcf62624f8225ce05875
annared <annared1982@gmail.com>
2023-11-13 10:44:46 +0200

    added files

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,6 +2,18 @@ const assert = require("assert");
 
 const { Given, When, Then } = require("@cucumber/cucumber");
 
-module.exports = {
-  default: `--format-options '{"snippetInterface": "synchronous"}'`,
-};
+function isItFriday(today) {
+  return today === "Friday" ? "TGIF" : "Nope";
+}
+
+Given("today is {string}", function (string) {
+  this.today = string;
+});
+When("I ask whether it's Friday yet", function () {
+  this.actualAnswer = isItFriday(this.today);
+});
+Then("I should be told {string}", function (expectedAnswer) {
+  console.log("expectedAnswer=", expectedAnswer);
+  console.log("this.actualAnswer=", this.actualAnswer);
+  assert.strictEqual(this.actualAnswer, expectedAnswer);
+});
